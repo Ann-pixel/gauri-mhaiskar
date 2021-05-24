9c400b1d82ffd5602f8c805d9be5f5e5e310ec40
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-05-24 17:05:58 -0400

    added more styling

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 // alert("Baaaaaaaaaaaaa! Youre on! 🎇");
-console.log("Hi there!");
+const aboutNav = document.querySelector(".about-nav");
+const projectsNav = document.querySelector(".projects-nav");
+const connectNav = document.querySelector(".connect-nav");
+
+function showAbout() {
+  projectsNav.classList.add("hidden");
+  connectNav.classList.add("hidden");
+  aboutNav.classList.remove("hidden");
+}
+function showProjects() {
+  aboutNav.classList.add("hidden");
+  connectNav.classList.add("hidden");
+  projectsNav.classList.remove("hidden");
+}
+function showConnect() {
+  projectsNav.classList.add("hidden");
+  showNav.classList.add("hidden");
+  connectNav.classList.remove("hidden");
+}
+
+aboutNav.addEventListener("click", showAbout);
+projectsNav.addEventListener("click", showProjects);
+connectNav.addEventListener("click", showConnect);
