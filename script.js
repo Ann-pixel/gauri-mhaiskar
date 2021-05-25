e0192448f56f420e59193fe417970164a684f6c5
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-05-25 18:31:20 -0400

    fixed css

--- script.js
+++ script.js
@@ -3,20 +3,26 @@ const aboutNav = document.querySelector(".about-nav");
 const projectsNav = document.querySelector(".projects-nav");
 const connectNav = document.querySelector(".connect-nav");
 
+const aboutSection = document.getElementById("about");
+const projectsSection = document.getElementById("projects");
+const connectSection = document.getElementById("connect");
 function showAbout() {
-  projectsNav.classList.add("hidden");
-  connectNav.classList.add("hidden");
-  aboutNav.classList.remove("hidden");
+  console.log("clicked about");
+  projectsSection.classList.add("hidden");
+  connectSection.classList.add("hidden");
+  aboutSection.classList.remove("hidden");
 }
 function showProjects() {
-  aboutNav.classList.add("hidden");
-  connectNav.classList.add("hidden");
-  projectsNav.classList.remove("hidden");
+  console.log("clicked project");
+  aboutSection.classList.add("hidden");
+  connectSection.classList.add("hidden");
+  projectsSection.classList.remove("hidden");
 }
 function showConnect() {
-  projectsNav.classList.add("hidden");
-  showNav.classList.add("hidden");
-  connectNav.classList.remove("hidden");
+  console.log("clicked connect");
+  projectsSection.classList.add("hidden");
+  aboutSection.classList.add("hidden");
+  connectSection.classList.remove("hidden");
 }
 
 aboutNav.addEventListener("click", showAbout);
